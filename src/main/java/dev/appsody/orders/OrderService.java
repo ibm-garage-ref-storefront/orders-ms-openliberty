4e15b78c107092005418ef55b5c3f53a70bbb83e
Hemankita Perabathini <Hemankita1@Hemankitas-MacBook-Pro-2.local>
2020-10-20 18:57:03 +0530

    orders get and post apis

---
+++ src/main/java/dev/appsody/orders/OrderService.java
@@ -0,0 +1,119 @@
+package dev.appsody.orders;
+
+import java.io.IOException;
+import java.security.Principal;
+import java.util.Calendar;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.TimeoutException;
+
+import javax.annotation.security.DeclareRoles;
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
+import dev.appsody.orders.model.Order;
+import dev.appsody.orders.utils.OrderDAOImpl;
+
+@RequestScoped
+@Path("/orders")
+public class OrderService {
+
+    @Inject
+    private JsonWebToken jwt;
+
+    @GET
+//    @RolesAllowed({"admin","user"})
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getOrders() throws Exception {
+        try {
+            if (jwt == null) {
+                // distinguishing lack of jwt from a poorly generated one
+                return Response.status(Response.Status.BAD_REQUEST).entity("Missing JWT").build();
+            }
+            else {
+            	System.out.println("MP JWT config message: " + jwt.getName() );
+                System.out.println("MP JWT getIssuedAtTime " + jwt.getIssuedAtTime() );
+            }
+            final String customerId = jwt.getName();
+            if (customerId == null) {
+                // if no user passed in, this is a bad request
+                // return "Invalid Bearer Token: Missing customer ID";
+                return Response.status(Response.Status.BAD_REQUEST).entity("Invalid Bearer Token: Missing customer ID from jwt: " + jwt.getRawToken()).build();
+            }
+
+            System.out.println("caller: " + customerId);
+
+            OrderDAOImpl ordersRepo = new OrderDAOImpl();
+
+            final List<Order> orders = ordersRepo.findByCustomerIdOrderByDateDesc(customerId);
+
+            return Response.ok(orders).build();
+
+        } catch (Exception e) {
+            System.err.println(e.getMessage() + "" + e);
+            System.err.println("Entering the Fallback Method from getOrders().");
+            throw new Exception(e.toString());
+        }
+
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+//    @RolesAllowed({ "admin" })
+    public Response create(
+        Order payload, @Context UriInfo uriInfo) throws IOException, TimeoutException {
+        try {
+            if (jwt == null) {
+                // distinguishing lack of jwt from a poorly generated one
+                return Response.status(Response.Status.BAD_REQUEST).entity("Missing JWT").build();
+            }
+            final String customerId = jwt.getName();
+            if (customerId == null) {
+                // if no user passed in, this is a bad request
+                //return "Invalid Bearer Token: Missing customer ID";
+                return Response.status(Response.Status.BAD_REQUEST).entity("Invalid Bearer Token: Missing customer ID from jwt: " + jwt.getRawToken()).build();
+            }
+
+            payload.setDate(Calendar.getInstance().getTime());
+            payload.setCustomerId(customerId);
+
+            String id = UUID.randomUUID().toString();
+
+            payload.setId(id);
+
+            System.out.println("New order: " + payload.toString());
+
+            OrderDAOImpl ordersRepo = new OrderDAOImpl();
+            ordersRepo.putOrderDetails(payload);
+
+            UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+            builder.path(payload.getId());
+            System.out.println(builder.build().toString());
+            
+            return javax.ws.rs.core.Response.created(builder.build()).build();
+
+        } catch (Exception ex) {
+            System.err.println("Error creating order: " + ex);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Error creating order: " + ex.toString()).build();
+        }
+
+    }
+
+}
+
---
+++ src/main/java/dev/appsody/orders/utils/OrderDAOImpl.java
@@ -0,0 +1,102 @@
+package dev.appsody.orders.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import dev.appsody.orders.model.Order;
+
+public class OrderDAOImpl {
+	
+	public List<Order> findByCustomerIdOrderByDateDesc(String customerId) {
+        List<Order> orderData = new ArrayList<>();
+        JDBCConnection jdbcConnection = new JDBCConnection();
+        Connection connection = jdbcConnection.getConnection();
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(
+                    "select orderId,itemId,customerId,count,date from ordersdb.orders where customerId=?");
+            ps.setString(1, customerId);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Order order = new Order();
+                order.setId(rs.getString("orderId"));
+                order.setItemId(rs.getInt("itemId"));
+                order.setCustomerId(rs.getString("customerId"));
+                order.setCount(rs.getInt("count"));
+                order.setDate(rs.getTimestamp("date"));
+                orderData.add(order);
+
+            }
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return orderData;
+    }
+
+
+    public List<Order> findByOrderId(String id) {
+
+        List<Order> orderData = new ArrayList<>();
+
+        JDBCConnection jdbcConnection = new JDBCConnection();
+
+        Connection connection = jdbcConnection.getConnection();
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(
+                    "select orderId,itemId,customerId,count,date from ordersdb.orders where orderId=?");
+            ps.setString(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Order order = new Order();
+                order.setId(rs.getString("orderId"));
+                order.setItemId(rs.getInt("itemId"));
+                order.setCustomerId(rs.getString("customerId"));
+                order.setCount(rs.getInt("count"));
+                order.setDate(rs.getTimestamp("date"));
+                System.out.println("added");
+                orderData.add(order);
+            }
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return orderData;
+    }
+
+    public void putOrderDetails(Order order) {
+
+        JDBCConnection jdbcConnection = new JDBCConnection();
+
+        Connection connection = jdbcConnection.getConnection();
+        try {
+            String query = " insert into ordersdb.orders (orderId,itemId,customerId,count,date)"
+                    + " values (?, ?, ?, ?, ?)";
+
+            PreparedStatement preparedStmt = connection.prepareStatement(query);
+            preparedStmt.setString(1, order.getId());
+            preparedStmt.setInt(2, order.getItemId());
+            preparedStmt.setString(3, order.getCustomerId());
+            preparedStmt.setInt(4, order.getCount());
+            preparedStmt.setTimestamp(5, new java.sql.Timestamp(new java.util.Date().getTime()));
+
+            preparedStmt.execute();
+
+            connection.close();
+        } catch (Exception e) {
+            System.err.println("Got an exception!");
+            System.err.println(e.getMessage());
+        }
+    }
+
+}
